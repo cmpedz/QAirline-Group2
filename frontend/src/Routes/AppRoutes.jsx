de244189291903d2cd718ef4917fa0dab971c188
hoamai1509 <hoamai1509@gmail.com>
2024-12-02 03:17:20 +0700

    Login, Signup, CurrentBooking
    update routes and navbar

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -3,8 +3,12 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Home from "../page/Home";
+import Login from "../page/Login";
+import SignUp from "../page/SignUp";
 import ErrorPage from "../page/ErrorPage";
+
 import TicketSearchPage from "../page/TicketSearchPage";
+import CurrentBooking from "../components/CurrentBooking";
 
 
 
@@ -14,7 +18,10 @@ const AppRoutes = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/book" element={<TicketSearchPage />} />
+        <Route path="/bookings" element={<CurrentBooking />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
