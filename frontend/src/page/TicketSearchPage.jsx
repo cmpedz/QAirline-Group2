8b4c04161c8b8de99ad38b7ef67024599ed495fd
Cong <125168053+buiftheescoong@users.noreply.github.com>
2024-12-16 21:48:44 +0700

    change UI ticket search page

--- frontend/src/page/TicketSearchPage.jsx
+++ frontend/src/page/TicketSearchPage.jsx
@@ -47,7 +47,9 @@ const TicketSearchPage = () => {
 
   return (
 
-    <div className="px-[30px] md:px-[30px] max-w-[1400px] mx-auto">
+    // <div className="px-[30px] md:px-[30px] max-w-[1400px] mx-auto">
+    <div className="max-w-[1400px] mx-auto">
+
       <BookTicketBox
         formData={formData}
         setFormData={setFormData}
