3712f097ce0651bf4ae4931074afee75d7a559b3
hoamai1509 <hoamai1509@gmail.com>
2024-12-21 20:33:49 +0700

    del HEAD

--- frontend/src/admin/components/SidebarItem.jsx
+++ frontend/src/admin/components/SidebarItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
 const handleLogout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
